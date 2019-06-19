347469790bc90adbf9182e99bb40c79009956816
abdallah awwad <a.awwad@procabnow.com>
2019-06-19 19:33:10 +0200

    add google map

---
+++ src/components/Dispatcher.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-container>
+    <h1>Hello</h1>
+
+    <GmapMap
+      :center="{ lat: 10, lng: 10 }"
+      :zoom="7"
+      map-type-id="terrain"
+      style="width: 500px; height: 300px"
+    >
+      <GmapMarker
+        :key="index"
+        v-for="(m, index) in markers"
+        :position="m.position"
+        :clickable="true"
+        :draggable="true"
+        @click="center = m.position"
+      />
+    </GmapMap>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      cities: []
+    };
+  }
+};
+</script>
+
+<style></style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
 import Vue from "vue";
+import * as VueGoogleMaps from "vue2-google-maps";
 import "./plugins/vuetify";
 import App from "./App.vue";
 import "roboto-fontface/css/roboto/roboto-fontface.css";
 import "material-design-icons-iconfont/dist/material-design-icons.css";
 
 Vue.config.productionTip = false;
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: "AIzaSyCgPrONYaAFeCYlESi1xGzCbODSZ5FScuE"
+  }
+});
 
 new Vue({
   render: h => h(App)
